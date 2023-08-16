5105a3384afa0cf5c3d4156420883fead316a6df
delissgt <delissgt@gmail.com>
2023-08-15 18:22:37 -0600

    Form data
    Retrieve Form Data using FastAPI Form

---
+++ exercises_chapter3/chapter3_form_data_01.py
@@ -0,0 +1,10 @@
+# install python-multipar previously
+
+from fastapi import FastAPI, Form
+
+app = FastAPI()
+
+
+@app.post("/users")
+async def create_user(name: str = Form(...), age: int = Form(...)):
+    return {"name": name, "age": age}
