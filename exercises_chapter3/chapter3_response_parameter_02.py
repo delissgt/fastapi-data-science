5cdd6a502fac9c5885ef28270ba44f10db72288b
delissgt <delissgt@gmail.com>
2023-08-18 18:33:50 -0600

    Set Cookie
    Find this cookie in cookies in brower with the name cookie name this will habe a life of 86400 seconds

---
+++ exercises_chapter3/chapter3_response_parameter_02.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Response
+
+app = FastAPI()
+
+
+@app.get("/")
+async def custom_cookie(response: Response):
+    response.set_cookie("cookie_name", "cookie_value", max_age=86400)
+    return {"hello": "word"}
